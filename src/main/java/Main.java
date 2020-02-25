bf26141b7a6c1b48b58f1957f40cbb9adfcc74e7
tpazz <tkoorehpaz1@sheffield.ac.uk>
2020-02-25 01:02:24 +0000

    Implemented file I/O for progress

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,6 @@
 import com.googlecode.lanterna.TerminalSize;
-import com.googlecode.lanterna.TextColor;
 import com.googlecode.lanterna.gui2.*;
 import com.googlecode.lanterna.gui2.dialogs.ActionListDialogBuilder;
-import com.googlecode.lanterna.gui2.dialogs.MessageDialog;
 import com.googlecode.lanterna.gui2.dialogs.MessageDialogBuilder;
 import com.googlecode.lanterna.gui2.dialogs.MessageDialogButton;
 import com.googlecode.lanterna.screen.Screen;
@@ -11,10 +9,8 @@ import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
 import com.googlecode.lanterna.terminal.Terminal;
 
 import java.io.*;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.TimeZone;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 public class Main {
 
@@ -26,101 +22,124 @@ public class Main {
         Screen screen = new TerminalScreen(terminal);
         screen.startScreen();
 
-        BasicWindow window = new BasicWindow();
-
         final WindowBasedTextGUI textGUI = new MultiWindowTextGUI(screen);
-
-        new MessageDialogBuilder()
-                .setTitle("Acknowledgement")
-                .setText("\n" + "This is a password cracking game." + "\n" +
-                         "All of the techniques used in this game" + "\n" +
-                         "have been crafted to accurately resemble" + "\n" +
-                         "sophisticated cracking algorithms and" + "\n" +
-                         "how they can be used against legitimate" + "\n" +
-                         "password data sets." + "\n" +
-                         "\n" + "The objective of this game is to increase" + "\n" +
-                         "password security and awareness and not to " + "\n" +
-                         "promote hacking in any way." + "\n" +
-                         "\n" + "Enjoy!")
-                .addButton(MessageDialogButton.OK)
-                .build()
-                .showDialog(textGUI);
-
-        //clearFile();
-        //writeFile(8,9.12f);
-        newGameMenu(textGUI);
+        message(textGUI, "Acknowledgement", ACKNOWLEDGEMENT);
+        menu(textGUI);
     }
 
-    private static void newGameMenu(WindowBasedTextGUI gui) {
-        new ActionListDialogBuilder()
-                .setTitle("Main Menu")
-                .setDescription("\n")
-                .addAction("New Game", new Runnable() {
-                    @Override
-                    public void run() {
-                        // Do 1st thing...
-                    }
-                })
-                .addAction("How to play", new Runnable() {
-                    @Override
-                    public void run() {
-                        // Do 2nd thing...
-                    }
-                })
-                .build()
-                .showDialog(gui);
-    }
-
-    private static void contGameMenu(WindowBasedTextGUI gui) {
-        new ActionListDialogBuilder()
-                .setTitle("Main Menu")
-                .setDescription("\n")
-                .addAction("Continue", new Runnable() {
-                    @Override
-                    public void run() {
-                        // Do 1st thing...
-                    }
-                })
-                .addAction("New game", new Runnable() {
-                    @Override
-                    public void run() {
-                        // Do 2nd thing...
-                    }
-                })
-                .addAction("How to play", new Runnable() {
-                    @Override
-                    public void run() {
-                        // Do 3rd thing...
-                    }
-                })
-                .build()
-                .showDialog(gui);
-    }
-
-    private static void clearFile() throws Exception {
-        File file = new File("src\\main\\java\\progress.txt");
-        PrintWriter writer = new PrintWriter(file);
-        writer.print("");
-        writer.close();
-    }
-
-    private static void writeFile(int i, float f) throws Exception {
-        String level, bitcoin;
-        if (file.length() == 0) {
-            FileOutputStream fos = new FileOutputStream(file);
-            DataOutputStream dos = new DataOutputStream(fos);
-            dos.writeInt(i);
-            dos.writeFloat(f);
+    private static void menu(WindowBasedTextGUI gui) throws Exception {
+        if (getLevel().equals("0")) { // fresh start or selecting 'New Game' will omit 'Continue' option
+            new ActionListDialogBuilder()
+                    .setTitle("MENU")
+                    .setDescription("\n")
+                    .addAction("New game", () -> {
+                        try {
+                            resetProgress();
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                    })
+                    .addAction("How to play", () -> message(gui, "HOW TO PLAY", HOW2PLAY))
+                    .addAction("About", () -> message(gui, "ABOUT", ABOUT))
+                    .setCanCancel(false)
+                    .build()
+                    .showDialog(gui);
         }
         else {
-            BufferedReader br = new BufferedReader(new FileReader(file));
-            level = br.readLine();
-            bitcoin = br.readLine();
-            clearFile();
-            FileOutputStream fos = new FileOutputStream(file);
-            DataOutputStream dos = new DataOutputStream(fos);
-            dos.writeInt(Integer.parseInt(level) + i);
-            dos.writeFloat(Float.parseFloat(bitcoin + f));
+            new ActionListDialogBuilder()
+                    .setTitle("Main Menu")
+                    .setDescription("\n")
+                    .addAction("Continue", () -> {
+
+                    })
+                    .addAction("New game", () -> {
+                        try {
+                            resetProgress();
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                    })
+                    .addAction("How to play", () -> message(gui, "HOW TO PLAY", HOW2PLAY))
+                    .addAction("About", () -> message(gui, "ABOUT", ABOUT))
+                    .setCanCancel(false)
+                    .build()
+                    .showDialog(gui);
         }
     }
+
+    // helper messageDialogBuilder to display custom message
+    private static void message(WindowBasedTextGUI gui, String title, String message) {
+        new MessageDialogBuilder()
+                .setTitle(title)
+                .setText("\n" + message)
+                .addButton(MessageDialogButton.OK)
+                .build()
+                .showDialog(gui);
+        try {
+            menu(gui);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    // file I/O helpers ////////////////////////////////////////////////////////////////////////////////////////////////
+    private static void resetProgress() throws Exception {
+        FileWriter fw = new FileWriter(file);
+        fw.write("0" + "\n" + "0.0");
+        fw.close();
+    }
+
+    public static void writeFile(int i, float f) throws Exception {
+        int level = i + Integer.valueOf(getLevel());
+        float bitcoin = f + Float.valueOf(getBitcoin());
+        FileWriter fw = new FileWriter(file);
+        fw.write(Integer.toString(level) + "\n" + Float.toString(bitcoin));
+        fw.close();
+    }
+
+    public static String getLevel() throws Exception {
+        return Files.readAllLines(Paths.get(String.valueOf(file))).get(0);
+    }
+
+    public static String getBitcoin() throws Exception {
+        return Files.readAllLines(Paths.get(String.valueOf(file))).get(1);
+    }
+
+    // long text constants /////////////////////////////////////////////////////////////////////////////////////////////
+    private final static String HOW2PLAY =
+            "You are playing the role of a black hat hacker that cracks passwords" + "\n" +
+            "from profiles in exchange for \u20BFitcoin." +"\n\n" +
+            "Profiles are randomly generated with an associated password that" + "\n" +
+            "is related to the profile in some way, for instance:" + "\n\n" +
+            "First name: John" + "\n" +
+            "Last name: Smith" + "\n" +
+            "DoB: 15/07/1988" + "\n\n" +
+            "Password: j0hnSm1th88" + "\n\n" +
+            "Passwords will become more challenging to crack as you progress" + "\n" +
+            "but will yield a higher \u20BF reward. Profiles can be requested" + "\n" +
+            "in exchange for a small deposit fee which is lost if you choose to" + "\n" +
+            "delete the request, and up to 3 profiles can be held at a time." + "\n\n" +
+            "\u20BF can be used to purchase essential cracking tools that will" + "\n" +
+            "aid you in cracking passwords.";
+
+
+    private final static String ABOUT =
+            "This artifact is part of an undergraduate research project led by" + "\n" +
+            "Theo Koorehpaz at the University of Sheffield. The objective is to" + "\n" +
+            "increase password security and awareness by the means of Gamification." + "\n\n" +
+            "Supervisor: Ramsay Taylor" + "\n" +
+            "Module code: COMxxx" + "\n" +
+            "Department of Computer Science";
+
+    private final static String ACKNOWLEDGEMENT =
+            "This is a password cracking game." + "\n" +
+            "All of the techniques used in this game" + "\n" +
+            "have been crafted to accurately resemble" + "\n" +
+            "sophisticated cracking algorithms and" + "\n" +
+            "how they can be used against legitimate" + "\n" +
+            "password data sets." + "\n" +
+            "\n" + "The objective of this game is to increase" + "\n" +
+            "password security and awareness and not to " + "\n" +
+            "promote hacking in any way." + "\n" +
+            "\n" + "Enjoy!";
 }
